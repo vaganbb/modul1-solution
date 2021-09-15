5a8b6a0c6bf1e92d5951a3c508ad1c332972e8c8
Poletanovic <vaganbb@gmail.com>
2021-09-15 17:12:45 +0200

    corrected version of assigment 1

--- app.js
+++ app.js
@@ -5,15 +5,32 @@
     .controller('LunchCheckController', LunchCheckController);
     
     LunchCheckController.$inject = ['$scope'];
+
     function LunchCheckController($scope) {
-       $scope.name = "Brani";
-       $scope.placeText = "Banana, apple, orange";
-    
-   
+       $scope.name = "Please enter your dish wish list.";
+
        $scope.feedLunch = function () {
-         $scope.name = "Cevap";
-       };
-     }
+          if ($scope.dishList == undefined){
+             return $scope.name = "Please check your input!";
+          }
+
+         var numOfDishes = countDishlist($scope.dishList);
+         
+         if (numOfDishes < 4){
+            return $scope.name = "Enjoy!";
+         }
+         else {
+            return $scope.name = "Too much!";
+         }
+      };
+
+      function countDishlist(string){
+         var dishValue;
+         dishValue = string.split(",").length;
+         return dishValue;
+      };
+
+   };
     
     })();
     
